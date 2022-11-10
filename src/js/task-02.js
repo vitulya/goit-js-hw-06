6a43061fe7b05c9c29554c539ddcfc6347386da8
vitulya <vitulya.bondarchuk@gmail.com>
2022-11-10 23:50:49 +0200

    task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+function createItemIngredients(array) {
+
+    const itemsArr = array.map(element => {
+    const item = document.createElement('li');
+    item.textContent = element;
+    item.classList.add('item');
+    
+      return item;
+    })
+    
+  ingredientsList.append(...itemsArr);
+}
+// createItemIngredients(ingredients)
+createItemIngredients(ingredients);
