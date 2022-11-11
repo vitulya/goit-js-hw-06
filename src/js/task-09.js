c6ff4140f11954f7517aa7b2b81e28c30c0c54e0
vitulya <vitulya.bondarchuk@gmail.com>
2022-11-11 19:13:33 +0200

    Task9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(3, 0)}`;
 }
+
+const button = document.querySelector('.change-color');
+const output = document.querySelector('.color')
+
+button.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  output.textContent = color;
+  document.body.style.backgroundColor = color;
+});
+
+
+
