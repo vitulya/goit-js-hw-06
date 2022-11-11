40a0020b98ac03d192ed703b9f5f467f8bb3b0fc
vitulya <vitulya.bondarchuk@gmail.com>
2022-11-11 18:57:05 +0200

    Task8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,26 @@
+const form = document.querySelector('.login-form');
+const button = document.querySelector('[type="submit"]');
+// console.log(form.elements.email)
+//   console.log(form.elements.password)
+
+
+button.addEventListener('click', function (event) {
+    event.preventDefault();
+    const { elements: { email, password } } = form;
+
+    console.log(email);
+    
+    if (email.value === '' || password.value === '') {
+       alert ('Всі поля повинні бути заповнені') 
+    }
+
+    const user = {
+        [email.name]: email.value,
+        [password.name]: password.value,
+    }
+
+    console.log(user);
+
+    form.reset();
+})
 
